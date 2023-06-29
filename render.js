3d8763967ed1bcbc448bd5202999d6332a8f3231
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-06-29 19:19:21 +0300

    Remove unneeded buttons

--- render.js
+++ render.js
@@ -3,10 +3,11 @@ const Renderer = function () {
     const renderPosts = function (posts) {
         const postsElement = $("#posts")
         postsElement.empty()
-        for(const post of posts)
-        {
-            const postTextElement = postsElement.append(`<div data-id="${post.id}" id="${post.id}" class="post-text"></div>`)
-            $(`#${[post.id]}`).text(post.text)
+        for (const post of posts) {
+            const postTextElement = postsElement.append(
+                `<div data-id="${post.id}" id="${post.id}" class="post-text"></div>`
+            )
+            $(`#${post.id}`).text(post.text)
         }
     }
     return { renderPosts }
