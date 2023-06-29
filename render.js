7fc27d30a710c94be4e381dbdab2d65707f0fce4
Rostislav Rusakov <134939763+rostrskv@users.noreply.github.com>
2023-06-29 21:42:50 +0300

    Add comment rendering

--- render.js
+++ render.js
@@ -4,17 +4,28 @@ const Renderer = function () {
         const postsElement = $("#posts")
         postsElement.empty()
         for (const post of posts) {
-            const postTextElement = postsElement.append(
-                `<div class="post"  data-id="${post.id}" id="${post.id}">                
+            postsElement.append(`
+<div class="post" data-id="${post.id}" id="${post.id}">
 <div class="post-text"></div>
 <button class="delete">\u00D7 Delete post</button>
 <input type="text" class="comment-input">
 <button class="comment-button">Comment</button>
 <div class="comments"></div>
 </div>
-`
-            )
-            $(`#${post.id}`).find(".post-text").text(post.text)
+`)
+            const postElement = $(`#${post.id}`)
+            postElement.find(".post-text").text(post.text)
+
+            const commentsElement = postElement.find(".comments")
+            for (const comment of post.comments) {
+                commentsElement.append(`
+<div class="comment"  data-id="${comment.id}" id="${comment.id}">
+<span class="comment-text"></span>
+<span class="delete-comment" title="Delete comment">\u00D7</span>
+</div>
+`)
+                postElement.find(`#${comment.id}`).find(".comment-text").text(comment.text)
+            }
         }
     }
     return { renderPosts }
